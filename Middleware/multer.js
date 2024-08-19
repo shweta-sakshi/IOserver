17b0f941ea529208ff62a0ffa2d524d355fdeeaa
shweta-sakshi <shweta25sakshi@gmail.com>
2024-08-20 02:34:07 +0530

    update path in mutler

--- Middleware/multer.js
+++ Middleware/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './utils/StoreFiles')
+        cb(null, '../utils/StoreFile')
     },
     filename: function (req, file, cb) {
         console.log("inside multer")
